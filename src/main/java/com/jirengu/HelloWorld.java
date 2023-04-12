b091c32c52c9feb4939881393f9fdb73bd879207
zhangge208 <zhangge208@gmail.com>
2023-04-13 02:44:11 +0800

    feat(hello-world): implement hello world

---
+++ src/main/java/com/jirengu/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.jirengu;
+
+public class HelloWorld {
+    // 在这里创建一个主方法, 打印Hello World
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/jirengu/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package com.jirengu;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void importStringUtilsCorrectly() {
+        HelloWorld.main(null);
+    }
+}
