38b6f2d17ade63fe5555c30814c5263259204571
zhangge208 <zhangge208@gmail.com>
2023-06-25 15:39:16 +0800

    Update HelloWorldTest.java

--- src/test/java/com/jirengu/HelloWorldTest.java
+++ src/test/java/com/jirengu/HelloWorldTest.java
@@ -3,8 +3,29 @@ package com.jirengu;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
+
     @Test
-    public void importStringUtilsCorrectly() {
-        HelloWorld.main(null);
+    public void testPrintHelloWorld() {
+        // 保存标准输出流的引用
+        PrintStream originalOut = System.out;
+
+        try {
+            // Arranage
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            PrintStream printStream = new PrintStream(outputStream);
+
+            System.setOut(printStream);
+
+            // Act
+            HelloWorld.main(null);
+
+            // Assert: 验证输出是否为 "Hello World"
+            String output = outputStream.toString().trim();
+
+            assertEquals("Hello World", output);
+        } finally {
+            // 恢复标准输出流
+            System.setOut(originalOut);
+        }
     }
 }
