fb92dce4478e46e6e93d402c8b2a53f69b640e36
zhangge208 <zhangge208@gmail.com>
2023-06-25 14:02:11 +0800

    Update HelloWorld.java

--- src/main/java/com/jirengu/HelloWorld.java
+++ src/main/java/com/jirengu/HelloWorld.java
@@ -3,6 +3,6 @@ package com.jirengu;
 public class HelloWorld {
     // 在这里创建一个主方法, 打印Hello World
     public static void main(String[] args) {
-
+        System.out.println("Hello World");
     }
 }
